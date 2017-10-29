8b931625be53aaaeea51298da9ec141502d499d3
nikitc <nikitc6@rambler.ru>
2017-10-29 16:01:55 +0500

    add project

---
+++ Homework2/MeasureTimeMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.AspNetCore.Http;
+
+namespace Homework2
+{
+    public class MeasureTimeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MeasureTimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var timer = new Timer();
+            timer.Start();
+            var token = context.Request.Query["token"];
+            timer.Stop();
+            await context.Response.WriteAsync($"Class MiddleWare {timer.Interval}\n");
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ Homework2/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System.Timers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Homework2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMiddleware<MeasureTimeMiddleware>();
+            app.Use(async (context, next) =>
+            {
+                var timer = new Timer();
+                timer.Start();
+                var token = context.Request.Query["token"];
+                timer.Stop();
+                await context.Response.WriteAsync($"Function MiddleWare {timer.Interval}\n");
+                await next.Invoke();
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
